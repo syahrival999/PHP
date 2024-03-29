3e14215f7256b1567a73a6cecde2b633a406a642
syahrival999 <100987662+syahrival999@users.noreply.github.com>
2024-03-30 01:01:06 +0700

    Add files via upload

---
+++ array/menampilkan.php
@@ -0,0 +1,8 @@
+<?php
+// membuat array
+$barang = ["Buku Tulis", "Penghapus", "Spidol"];
+
+// menampilkan isi array
+echo $barang[0]."<br>";
+echo $barang[1]."<br>";
+echo $barang[2]."<br>";
---
+++ belajar/menggabungkan-text.php
@@ -0,0 +1,10 @@
+<?php
+$txt1 = "Tutorial PHP";
+$txt2 = "petanikode.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Aku belajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
